07499428b0e8ffa6bd77b6f11daa1c4205d89e2d
Md Mohosin <mohosinali075@gmail.com>
2025-11-21 04:06:57 +0600

    remove module

--- dist/helpers/bull-mq-producer.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// import { Queue } from "bullmq";
-// import { redisClient } from "./redis";
-// export const notificationQueue = new Queue('notifications', {
-//   connection: redisClient
-// })
-// export const emailQueue = new Queue('emails', {
-//   connection: redisClient
-// })
