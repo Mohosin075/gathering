0bad80754e911411d0df3c143bf2db5b646ffa93
Md Mohosin <mohosinali075@gmail.com>
2025-12-02 23:27:39 +0600

    update event status

--- src/app/modules/event/event.validation.ts
+++ src/app/modules/event/event.validation.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod'
+import { EVENT_STATUS } from '../../../enum/event'
 
 export const EventValidations = {
   create: z.object({
@@ -9,8 +10,16 @@ export const EventValidations = {
       tags: z.array(z.string()).default([]),
       organizerId: z.string().optional(),
       status: z
-        .enum(['draft', 'published', 'cancelled', 'archived'])
-        .default('draft'),
+        .enum([
+          EVENT_STATUS.PENDING,
+          EVENT_STATUS.PUBLISHED,
+          EVENT_STATUS.CANCELLED,
+          EVENT_STATUS.ARCHIVED,
+          EVENT_STATUS.APPROVED,
+          EVENT_STATUS.COMPLETED,
+          EVENT_STATUS.REJECTED,
+        ])
+        .default(EVENT_STATUS.PENDING),
       visibility: z.enum(['public', 'private', 'unlisted']).default('public'),
       startDate: z.string(),
       startTime: z.string(),
@@ -33,31 +42,43 @@ export const EventValidations = {
   }),
 
   update: z.object({
-    body: z.object({
-      title: z.string().optional(),
-      description: z.string().optional(),
-      tags: z.array(z.string()).optional(),
-      status: z.enum(['published', 'cancelled', 'archived']).optional(),
-      visibility: z.enum(['public', 'private', 'unlisted']).optional(),
-      startDate: z.string().optional(),
-      startTime: z.string().optional(),
-      timezone: z.string().optional(),
-      locationType: z.enum(['physical', 'online']).optional(),
-      location: z
-        .object({
-          type: z.literal('Point'),
-          coordinates: z.array(z.number()).length(2),
-        })
-        .optional(),
-      address: z.string().optional(),
-      meetingLink: z.string().optional(),
-      capacity: z.number().optional(),
-      ticketsSold: z.number().optional(),
-      ticketPrice: z.number().optional(),
-      images: z.array(z.string()).optional(),
-      gallery: z.array(z.string()).optional(),
-      views: z.number().optional(),
-      favorites: z.number().optional(),
-    }).strict(),
+    body: z
+      .object({
+        title: z.string().optional(),
+        description: z.string().optional(),
+        tags: z.array(z.string()).optional(),
+        status: z
+          .enum([
+            EVENT_STATUS.PENDING,
+            EVENT_STATUS.PUBLISHED,
+            EVENT_STATUS.CANCELLED,
+            EVENT_STATUS.ARCHIVED,
+            EVENT_STATUS.APPROVED,
+            EVENT_STATUS.COMPLETED,
+            EVENT_STATUS.REJECTED,
+          ])
+          .optional(),
+        visibility: z.enum(['public', 'private', 'unlisted']).optional(),
+        startDate: z.string().optional(),
+        startTime: z.string().optional(),
+        timezone: z.string().optional(),
+        locationType: z.enum(['physical', 'online']).optional(),
+        location: z
+          .object({
+            type: z.literal('Point'),
+            coordinates: z.array(z.number()).length(2),
+          })
+          .optional(),
+        address: z.string().optional(),
+        meetingLink: z.string().optional(),
+        capacity: z.number().optional(),
+        ticketsSold: z.number().optional(),
+        ticketPrice: z.number().optional(),
+        images: z.array(z.string()).optional(),
+        gallery: z.array(z.string()).optional(),
+        views: z.number().optional(),
+        favorites: z.number().optional(),
+      })
+      .strict(),
   }),
 }
