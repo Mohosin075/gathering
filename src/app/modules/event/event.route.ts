2bb21f8413c35ece51502971529dab6f671d373f
Md Mohosin <mohosinali075@gmail.com>
2025-12-12 06:04:50 +0600

    remove auth fromt all event get

--- src/app/modules/event/event.route.ts
+++ src/app/modules/event/event.route.ts
@@ -10,7 +10,7 @@ const router = express.Router()
 
 router.get(
   '/',
-  auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN, USER_ROLES.ORGANIZER),
+  // auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN, USER_ROLES.ORGANIZER),
   EventController.getAllEvents,
 )
 router.get(
