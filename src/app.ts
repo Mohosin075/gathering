5eecacbf1e377a13a7e336475c111a272085ee4d
Md Mohosin <mohosinali075@gmail.com>
2026-06-28 05:25:33 +0600

    feat: initialize Express application with middleware, CORS, passport, and routing configurations

--- src/app.ts
+++ src/app.ts
@@ -39,7 +39,8 @@ app.use(
   cors({
     origin: [
       '*',
-      
+      'https://gatheringww.com',
+      'https://dashboard.gatheringww.com',
       'http://localhost:3000',
       'http://localhost:3001',
       'http://localhost:3002',
